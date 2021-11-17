06b3ddfa1b768af43504c70d951ba6d9721cdb35
ryan-sliger <ryan.sliger22@sitechhs.com>
2021-11-17 11:47:44 -0500

    class

---
+++ api-p/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+fetch('https://rickandmortyapi.com/api/episode')
+.then(data => {
+return data.json();
+})
+.then(data => {
+console.log(data);
+});
+
+</script>
+
